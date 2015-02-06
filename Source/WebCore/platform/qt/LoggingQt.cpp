f94bbbf322ff1f7bdcd1dafc7e627290e70b5439
Philippe Normand <philn@igalia.com>
2015-02-06 10:17:05 +0100

    [Qt] enable logging in all builds

--- Source/WebCore/platform/qt/LoggingQt.cpp
+++ Source/WebCore/platform/qt/LoggingQt.cpp
@@ -41,9 +41,6 @@ void initializeLoggingChannelsIfNecessary()
     if (loggingEnv.isEmpty())
         return;
 
-#if defined(NDEBUG)
-    qWarning("This is a release build. Setting QT_WEBKIT_LOG will have no effect.");
-#else
     QStringList channels = QString::fromLocal8Bit(loggingEnv).split(QLatin1String(","));
     for (int i = 0; i < channels.count(); i++) {
         if (WTFLogChannel* channel = getChannelFromName(channels.at(i)))
@@ -53,7 +50,6 @@ void initializeLoggingChannelsIfNecessary()
     // By default we log calls to notImplemented(). This can be turned
     // off by setting the environment variable DISABLE_NI_WARNING to 1
     LogNotYetImplemented.state = WTFLogChannelOn;
-#endif
 }
 
 } // namespace WebCore
